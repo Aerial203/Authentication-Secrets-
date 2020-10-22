6a1b3377d002f0c1a354853873d45cc43b3795bf
Ankit Shrestha <ankit.shrestha196@gmail.com>
2020-10-22 14:57:40 +0545

    Encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
+const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -22,8 +22,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-// const secret ="Thisisourlittlesecret";
-// userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+const secret ="Thisisourlittlesecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
